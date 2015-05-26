1d19c3570315b56ef616904c0488fb06d2cedd1f
Phan Long <longluudan@foobla.com>
2015-05-27 05:30:52 +0700

    update

--- js/custom-script.js
+++ js/custom-script.js
@@ -6,7 +6,16 @@ jQuery(document).ready( function($) {
 			$(this).removeClass('placeholder');
 		}
 	});
-
+	$(window).scroll(function () {
+		var offset = $('.top-content').offset();
+		if (offset.top > 2) {
+			$('.top-content').addClass('navbar-fixed-top');
+			$('.top-content').removeClass('no-affix');
+		} else {
+			$('.top-content').removeClass('navbar-fixed-top');
+			$('.top-content').addClass('no-affix');
+		}
+	});
 
 	//tab page setting 
 	$('.list-xem-theo li').click(function(e){
@@ -15,4 +24,63 @@ jQuery(document).ready( function($) {
 		$('.box-list-xem-theo .list-xem-theo-items').hide();
 		$('.box-list-xem-theo .list-xem-theo-items').eq($(this).index()).show();
 	});
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 2) {
+			$('#masthead.sticky-header .navigation').addClass('affix');
+			$('#masthead.sticky-header .navigation').removeClass('affix-top');
+		} else {
+			$('#masthead.sticky-header .navigation').removeClass('affix');
+			$('#masthead.sticky-header .navigation').addClass('affix-top');
+		}
+	});
 });
+(function ($) {
+	function load_masonry() {
+		var $blog = $('.list-article.masonry .content-inner-page');
+		if ($('.list-article.masonry .content-inner-page').length) {
+			$blog.imagesLoaded(function () {
+				$blog.isotope({
+					itemSelector: '.post-item'
+				});
+				// if ($(".page-content-inner").hasClass("scroll")) {
+				// 	$blog.infinitescroll({
+				// 			navSelector  : '.loop-pagination', // selector for the paged navigation
+				// 			nextSelector : '.loop-pagination a:first', // selector for the NEXT link (to page 2)
+				// 			extraScrollPx: 120,
+				// 			itemSelector : 'article.type-post', // selector for all items you
+				// 			animate      : true,
+				// 			bufferPx     : 40,
+				// 			errorCallback: function () {
+				// 			},
+				// 			infid        : 0, //Instance ID
+				// 			loading      : {
+				// 				finished   : undefined,
+				// 				finishedMsg: 'No more pages to load.',
+				// 				img        : "http://i.imgur.com/qkKy8.gif",
+				// 				msgText    : "<em>Loading the next set of posts...</em>",
+				// 				speed      : 'fast',
+				// 				start      : undefined
+				// 			}
+				// 		},
+				// 		function (newElements) {
+				// 			$blog.isotope('appended', jQuery(newElements));
+				// 			$blog.imagesLoaded(function () {
+				// 				$blog.isotope('layout');
+				// 			});
+				// 		});
+				// }
+			});
+		}
+	}
+	$(window).load(function () {
+		load_masonry();
+	});
+	$('.top-content .nav-tabs a').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+		load_masonry();
+	});
+	$('.checkbox input[type="checkbox"]').rsCheckAble();
+	$('.radio-inline input[type="radio"]').rsCheckAble();
+})(jQuery);
---
+++ js/jquery.checkable/jquery.rs.checkable.js
@@ -0,0 +1,99 @@
+﻿/*
+ * Plugin name: Redsand Jquery Custom Check Able Control
+ * Author: phannhuthan
+ * Uri: http://redsand.vn
+ * Version: 3.4.2
+ * Last modify: 12/15/2014
+ */
+
+/* param
+
+ options = {
+ change: function(event, ui = {checked, helper}){
+ ...
+ }
+ }
+
+ */
+
+(function($){
+	$.fn.rsCheckbox = $.fn.rsRadio = $.fn.rsCheckAble = function(options, checked, trigger){
+
+		if(options == 'checked'){
+			if(checked != undefined){
+				this.filter(':checkbox, :radio').each(function(){
+					if($(this).is('.rs-checkbox-hidden, .rs-radio-hidden')){
+						if($(this).is(':checked') != checked){
+							if(trigger || trigger == undefined){
+								$(this).attr('checked', checked).trigger('change');
+							}
+							else{
+								$(this).attr('checked', checked).next().toggleClass('checked', checked).parent('label').toggleClass('checked', checked);
+							}
+						}
+					}
+				});
+				return this;
+			}
+			else{
+				return this.is(':checked');
+			}
+		}
+
+		options = $.extend({
+			change: false,
+			text: ""
+		}, options);
+
+		this.filter(':checkbox, :radio').each(function(){
+			var type = $(this).is(':checkbox') ? 'checkbox' : 'radio';
+			var checkbox = $(this);
+			var helper = checkbox.next('.rs-' + type + '-trigger').removeClass('checked');
+			var label = checkbox.closest('label').removeClass('checked');
+
+			if(helper.length == 0){
+				helper = $('<a class="rs-' + type + '-trigger">' + options.text + '</a>');
+				checkbox.after(helper);
+			}
+
+			label.addClass('rs-' + type);
+			checkbox.addClass('rs-' + type + '-hidden');
+
+			if(checked) checkbox.attr('checked', true);
+
+			if(checkbox.is(':checked')) helper.add(label).addClass('checked');
+
+			if(checkbox.is(':disabled')) helper.add(label).addClass('disabled');
+
+			helper.unbind('click').click(function(event){
+				event.preventDefault();
+				if (!checkbox.is(':disabled')) {
+					checkbox.get()[0].click();
+				}
+			});
+
+			checkbox.unbind('change.rs-checkable').bind('change.rs-checkable', function (event) {
+				var name = checkbox.attr('name');
+				var list = checkbox.closest('form').length > 0 ? checkbox.closest('form').find('[name="' + name + '"]') : $('[name="' + name + '"]');
+				if (checkbox.is(':radio:checked')) {
+					list.not(this).removeAttr('checked').next().removeClass('checked').closest('label').removeClass('checked');
+				}
+				var checked = checkbox.is(':checked');
+				if(checked || list.filter(':checked').length ){
+					list.filter('[required]').removeAttr('required').addClass('rs-checkbox-required', true);
+				}
+				else{
+					list.filter('.rs-checkbox-required').attr('required', true);
+				}
+
+				helper.add(label).toggleClass('checked', checked);
+
+				if (typeof options.change == 'function') options.change.call(checkbox, event, {checked: checked, helper: helper});
+			});
+		});
+
+
+
+		return this;
+	}
+}(jQuery));
